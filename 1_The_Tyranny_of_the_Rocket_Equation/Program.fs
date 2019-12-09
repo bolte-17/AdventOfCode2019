22dc224a1ccffe81362e6c889a4fdc09577cfce3
bolte-17 <3034257+bolte-17@users.noreply.github.com>
2019-12-09 11:43:50 -0500

    Initial setup, Day 1 solution

---
+++ 1_The_Tyranny_of_the_Rocket_Equation/Program.fs
@@ -0,0 +1,14 @@
+﻿module Program
+
+open System.IO
+open Tests
+
+let totalFuel: seq<int> -> int = Seq.sumBy recFuel
+
+[<EntryPoint>]
+let main argv =
+    File.ReadLines(argv.[0])
+      |> Seq.map int
+      |> totalFuel
+      |> printfn "%d"
+    0 // return an integer exit code
---
+++ 1_The_Tyranny_of_the_Rocket_Equation/Tests.fs
@@ -0,0 +1,26 @@
+module Tests
+
+open System
+open Xunit
+
+let fuel mass = mass / 3 - 2;
+
+let rec recFuel mass = 
+  match fuel(mass) with 
+    | f when f > 0 -> f + recFuel(f)
+    | _ -> 0
+
+[<Theory>]
+[<InlineData(12,2)>]
+[<InlineData(14,2)>]
+[<InlineData(1969, 654)>]
+[<InlineData(100756, 33583)>]
+let ``Calcluates fuel requirements for given mass`` (input: int, expected: int) = 
+  Assert.Equal(expected, fuel(input))
+
+[<Theory>]
+[<InlineData(14,2)>]
+[<InlineData(1969,966)>]
+[<InlineData(100756, 50346)>]
+let ``Calcluates fuel requirements including fuel`` (input: int, expected: int) = 
+  Assert.Equal(expected, recFuel(input))
